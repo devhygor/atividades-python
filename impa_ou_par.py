d53d39d09078954c647da39d9b97639ea387443b
Hygor Rocha <hygor.k92@gmail.com>
2024-02-17 00:01:39 -0300

    exercicio para verificar se o numero é impa ou par

---
+++ impa_ou_par.py
@@ -0,0 +1,21 @@
+def input_numero_impa_ou_par():
+    numero = int(input('Digite um numero para verificar se é impar ou par: '))
+    return numero
+
+def verfica_se_o_numero_e_impa_ou_par(numero):
+    if numero % 2 == 0:
+        resultado = f'O número {numero} é PAR'
+    else:
+        resultado = f'O número {numero} é IMPA'
+    return resultado
+
+def exibe_o_resultado(resultado):
+    print(resultado)
+
+def main():
+    numero = input_numero_impa_ou_par()
+    resultado = verfica_se_o_numero_e_impa_ou_par(numero)
+    exibe_o_resultado(resultado)
+
+if __name__ == '__main__':
+    main()

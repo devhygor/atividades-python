728d03a251655b5e3c367619f741002b4eabe12b
Hygor Rocha <hygor.k92@gmail.com>
2024-02-21 08:42:12 -0300

    percorrer lista com for

---
+++ atividades/percorrer_lista_com_for.py
@@ -0,0 +1,12 @@
+"""
+    Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+"""
+
+def percorrer_lista_com_for():
+    lista = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7']
+    
+    for item in lista:
+        print(item)
+        
+if __name__ == '__main__':
+    percorrer_lista_com_for()
